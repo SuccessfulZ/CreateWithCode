0813f728d18ae5a6be51d413b931965c44245f60
Yelyzaveta Melnyk <lizamelnyk@gmail.com>
2021-01-08 15:47:12 +0200

    Debug and Fix Chalange 2 prototype

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float timeout = 2f; // set here the time
+     private float waitTime = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Increase every frame
+        waitTime -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (waitTime < 0))
+        {        
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // reset time 
+            waitTime = timeout;
+        }
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    int ballAmount;
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DelayedSpawnRandomBall", Random.Range(3,5));
+        ballAmount = ballPrefabs.Length;
+    }
+     void DelayedSpawnRandomBall()
+    {
+        SpawnRandomBall();
+        Invoke("DelayedSpawnRandomBall",Random.Range(3, 5));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        int ballNumber = Random.Range(0, ballAmount);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballNumber], spawnPos, ballPrefabs[ballNumber].transform.rotation);
+    }
+
+}
