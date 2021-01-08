b626e0cd5e62b110e4bf93e2a0fb1497eb42edae
Yelyzaveta Melnyk <lizamelnyk@gmail.com>
2021-01-08 19:26:45 +0200

    Creation of own project
    set up camera and scene, start prototyping game, add object + materials regarding logic

---
+++ Personal Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    float rotationX;
+    float mouseSensitivity = 90f;
+    public GameObject player;
+    
+    void Start(){
+        //Hide mouse cursore
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        //Taking mouse input
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        //Rotate camera up and down
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        transform.localEulerAngles = new Vector3(rotationX, 0f, 0f);
+
+        // Rotate left & right
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
